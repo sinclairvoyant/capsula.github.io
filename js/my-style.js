36432b5c05ba4db8c8f5243515e40fb0b18409c2
sinclairvoyant <sinclairvoyant@gmail.com>
2015-11-14 23:03:47 -0600

    Updates to hero quotes, videos, body links

--- js/my-style.js
+++ js/my-style.js
@@ -4,13 +4,12 @@
   **********************************************************
   */
 
-  // Toggle tranparent navbar when the user scrolls the page
+ // Toggle tranparent navbar when the user scrolls the page
 
-  $(window).scroll(function() {
-    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
-    {
-        $('.opaque-navbar').addClass('opaque');
-    } else {
-        $('.opaque-navbar').removeClass('opaque');
-    }
-});
+ $(window).scroll(function () {
+     if ($(this).scrollTop() > 50) /*height in pixels when the navbar becomes non opaque*/ {
+         $('.opaque-navbar').addClass('opaque');
+     } else {
+         $('.opaque-navbar').removeClass('opaque');
+     }
+ });
