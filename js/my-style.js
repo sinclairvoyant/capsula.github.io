01286b55c53998acb0c9257e40e238bd2da72868
sinclairvoyant <sinclairvoyant@gmail.com>
2016-03-03 15:33:54 -0600

    Updated photos link to Instagram

--- js/my-style.js
+++ js/my-style.js
@@ -13,3 +13,15 @@
          $('.opaque-navbar').removeClass('opaque');
      }
  });
+
+// // Lightbox Gallery
+//
+// $('.thumbnail').click(function () {
+//     $('.modal-body').empty();
+//     var title = $(this).parent('a').attr("title");
+//     $('.modal-title').html(title);
+//     $($(this).parents('div').html()).appendTo('.modal-body');
+//     $('#myModal').modal({
+//         show: true
+//     });
+// });
